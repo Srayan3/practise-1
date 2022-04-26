4cd496644e0eb17b26aca33b202c16d5945422cf
Shan Abrar Srayan <93345251+Srayan3@users.noreply.github.com>
2022-04-26 21:07:54 +0600

    Add files via upload

---
+++ js/javaScript.js
@@ -0,0 +1,10 @@
+function functionOn() {
+    document.getElementById('on').src='img/on.jpg'
+    document.getElementById('button').style.display='none'
+    document.getElementById('button1').style.display='block'
+}
+function functionOff() {
+    document.getElementById('on').src='img/off.jpg'
+    document.getElementById('button').style.display='block'
+    document.getElementById('button1').style.display='none'
+}
